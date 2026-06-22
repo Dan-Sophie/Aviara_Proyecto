246320fbb62d133e7e77b56a43dfc34da8634e99
Sophie. <sophie510380@gmail.com>
2026-06-21 21:27:57 -0500

    Update settings.py

--- aviara/aviara_project/settings.py
+++ aviara/aviara_project/settings.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import os
 import sys
 GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,10 +65,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'aviara_project.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
