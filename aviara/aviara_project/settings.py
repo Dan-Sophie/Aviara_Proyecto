8a251cae86f4964fc46477e4f0b01155255f5e7b
Sophie. <sophie510380@gmail.com>
2026-06-21 21:08:07 -0500

    Update settings.py

--- aviara/aviara_project/settings.py
+++ aviara/aviara_project/settings.py
@@ -82,7 +82,8 @@ TIME_ZONE = 'America/Bogota'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'usuarios.Usuario'
 MEDIA_URL = '/media/'
